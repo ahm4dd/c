ea03574bd922e4417443ba940a55efec04e046a2
Ahmed Emad <ahmed.emad.edu@gmail.com>
2025-04-16 11:51:42 +0300

    Create simpleArrayCasting.c

---
+++ C learning/simpleArrayCasting.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void dump_graphics(graphics_t gsettings[10]) {
+  int *ptr = (int *)gsettings;
+  for (int i = 0; i < 30; i++) {
+    printf("settings[%d] = %d\n", i, ptr[i]);
+  }
+}
