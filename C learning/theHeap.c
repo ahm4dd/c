7436db1b503c4a3a93123990216ebd89c2f426f2
Ahmed Emad <ahmed.emad.edu@gmail.com>
2025-04-21 20:52:36 +0300

    Create theHeap.c

---
+++ C learning/theHeap.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *get_full_greeting(char *greeting, char *name, int size) {
+  char *full_greeting = malloc(size * sizeof(char));
+  snprintf(full_greeting, size, "%s %s", greeting, name);
+  return full_greeting;
+}
