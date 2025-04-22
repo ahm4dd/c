5974eb663f402efe624aa1fa00080c3bd202da45
Ahmed Emad <ahmed.emad.edu@gmail.com>
2025-04-22 18:24:46 +0300

    Create genericSwap.c

---
+++ C learning/genericSwap.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+
+void swap(void *vp1, void *vp2, size_t size) {
+  void * buffer = malloc(size);
+  if (buffer == NULL) {
+    return;
+  }
+  memcpy(buffer, vp1, size);
+  memcpy(vp1, vp2, size);
+  memcpy(vp2, buffer, size);
+  free(buffer);
+}
